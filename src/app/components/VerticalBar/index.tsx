a2ec558d4d2c4163b2ae4e671026267847989f78
sarkisovTu <sarkisov1337@gmail.com>
2022-10-12 16:13:17 +0300

    feat: VerticalBar component

---
+++ src/app/components/VerticalBar/index.tsx
@@ -0,0 +1,41 @@
+/**
+ *
+ * VerticalBar
+ *
+ */
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+const MenuWrapper = styled.div`
+  top: 50%;
+  transform: translateY(-50%);
+  position: fixed;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  height: fit-content;
+`;
+
+const MenuItem = styled.div`
+  width: 64px;
+  height: 64px;
+
+  & + & {
+    margin-top: 50px;
+  }
+`;
+
+interface Props {
+  components: React.ReactNode[];
+}
+
+export function VerticalBar(props: Props) {
+  console.log(props.components);
+  return (
+    <MenuWrapper>
+      {props.components.map(item => (
+        <MenuItem>{item}</MenuItem>
+      ))}
+    </MenuWrapper>
+  );
+}
